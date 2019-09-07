6b61c208f59f76eb09e33610be8bd83e80e301bf
beni28abc <troy.12178933@gmail.com>
2019-09-07 14:19:12 -0700

    commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
+import { PagesRoutingModule } from './pages-routing.module';
+import { StrategyComponent } from './strategy/strategy.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { DisclaimersComponent } from './disclaimers/disclaimers.component';
+import { OurteamComponent } from './ourteam/ourteam.component';
+import {PortfolioCardComponent} from './portfolio/portfolioCard.component';
+
+@NgModule({
+  declarations: [ StrategyComponent, PortfolioComponent, DisclaimersComponent, OurteamComponent, PortfolioCardComponent], 
+  imports: [
+    CommonModule, 
+    HomeModule,
+    PagesRoutingModule
+  ]
+})
+export class PagesModule { }
